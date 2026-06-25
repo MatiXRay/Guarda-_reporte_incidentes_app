3974b36e451c45dae4ad62d76fc12971d4a7388c
facundohenze <facundohenze@gmail.com>
2026-06-25 15:43:54 -0300

    idioma inputs

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { esES } from '@clerk/localizations'
 import { UserRoleProvider } from './context/UserRoleContext'
 import './index.css'
 import App from './App.tsx'
@@ -19,6 +20,14 @@ createRoot(document.getElementById('root')!).render(
           publishableKey={publishableKey}
           signInForceRedirectUrl="/dashboard"
           signUpForceRedirectUrl="/dashboard"
+          localization={{
+            ...esES,
+            formFieldInputPlaceholder__emailAddress: 'Correo electrónico',
+            formFieldInputPlaceholder__emailAddress_username: 'Correo electrónico',
+            formFieldInputPlaceholder__password: 'Contraseña',
+            formFieldInputPlaceholder__firstName: 'Nombre',
+            formFieldInputPlaceholder__lastName: 'Apellido',
+          }}
         >
         <UserRoleProvider>
           <App />
